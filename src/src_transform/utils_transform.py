45970c9a20a788ebf3566b6bbb0572237c1b880b
anas-elsafi <wadalsafi2015@gmail.com>
2025-07-18 15:39:19 +0100

    updated utils_transform.py, create dim_line data frame, dim_time done partially

--- src/src_transform/utils_transform.py
+++ src/src_transform/utils_transform.py
@@ -2,6 +2,7 @@ import boto3
 from pprint import pprint
 from datetime import datetime
 import json
+import pandas as pd
 
 def get_s3_extract_objects_list():
     s3_client = boto3.client("s3")
@@ -33,3 +34,39 @@ def get_latest_s3_object():
     s3_obj_dict = json.loads(s3_obj_bytes.decode('utf-8'))
     return s3_obj_dict
 
+
+def convert_s3_obj_to_df():
+    data = get_latest_s3_object()
+    df = pd.DataFrame(data)
+    #print(df.head())
+    return df   
+    
+
+def create_fact_line_status():
+    pass
+
+def create_dim_time():
+    data = convert_s3_obj_to_df()
+    data_copy = data.copy()
+    time_id = data_copy["modified"].head()
+    print(type(time_id))
+    #print(time_id)
+    
+    
+
+def create_dim_line():
+    data = convert_s3_obj_to_df()
+    data_copy = data.copy()
+    dim_line = data_copy.drop(columns=["modified", "status_severity","status_severity_description","time_created","reason"])
+    dim_line["line_name"] = dim_line["line_id"]
+    
+    #print(dim_line.head())
+    
+
+
+#convert_s3_obj_to_df()
+create_dim_time()
+
+
+#get_latest_s3_object()
+#'Key': '2025-07-14 10:37:53.526180_tfl_lines_status.json'
